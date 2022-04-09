277d507e8a0fca8338b59cef997db54deeddc23f
Robiul DS <robiulislam.ds@gmail.com>
2022-04-10 02:50:22 +0600

    Update ds-social-author-box.php

--- ds-social-author-box.php
+++ ds-social-author-box.php
@@ -41,8 +41,19 @@
  
     if ( !defined( 'ABSPATH' )) exit;
 	
-// admin area custome field create use action hook
-if ( is_admin() ){
-	require_once dirname( __FILE__ ).'/includes/admin/profile.php';
-}	
+function ds_user_admin_area_field( $methods){
+	$methods ['facebook'] = __('Facebook','dsrobi');
+	$methods ['twitter']  = __('Twiter','dsrobi');
+	$methods ['linkedin'] = __('LinkedIn','dsrobi');
+	$methods ['youtube'] = __('Youtube','dsrobi');
+	$methods ['github'] = __('GitHub','dsrobi');
+	$methods ['instagram'] = __('Instagram','dsrobi');
+	$methods ['tiktok'] = __('TikTok','dsrobi');
+	$methods ['pinterest'] = __('Pinterest','dsrobi');
+	$methods ['snapchat'] = __('Snapchat','dsrobi');
+	$methods ['other'] = __('Othere','dsrobi');
+	return $methods;	
+	
+}
+add_filter('user_contactmethods', 'ds_user_admin_area_field',);
 		
